c7b3f7f73b0402eae76a17729856579cabe6bb6d
fergusoa <aferguso@nrcan.gc.ca>
2017-02-07 10:53:38 -0500

    Updated the JoinCSVFiles.pl with smarter code that can cope with csv files with differing #'s of columns (and different order)

--- OptFramework/JoinCSVFiles.pl
+++ OptFramework/JoinCSVFiles.pl
@@ -5,41 +5,163 @@ $OUPTUT = "";
 open (WRITEOUT, ">JoinedCSVFiles.csv"); 
 
 
+my %masterheaders; 
+$masterheaders{"aaa-filename"} = 1 ; 
+my %NumRows = {}; 
+
+my %data = () ;
+
+# Parse input 
 for $file (@ARGV){
 
-  print " Processing : $file \n"; 
-  
-  
-  $LineNum = 1; 
-  
-  open ( READIN, $file ); 
-  
-  while ( my $line = <READIN> ){
-  
-    if ( $FirstFile == 1 && $LineNum == 1 ) {
+  print " reading data from : $file ... "; 
+  open (TSRESULTS, "$file") or fatalerror("Could not open ".getcwd()."/$file!");
 
-      print WRITEOUT "Filename,$line";
+  my $RowNumber = 0; 
+  my $firstline = 1;
+  my @headers;
+  my @numbers;
+ 
+  while ( my $line = <TSRESULTS> ){
+    
+    $line =~ s/\s+/ /g; 
+    $line =~ s/,\s*/,/g;
+    $line =~ s/^\s//g;
+    $line =~ s/,,/,/g; 
+    $line =~ s/\s*$//g; 
+    
+    if ( $firstline ){
       
-    }elsif( $LineNum > 1 ) {
+      @headers = split /,/, $line;  
+      $firstline = 0;
+      
+      my $index = 0;
+      for( $index=0; $index<$#headers; $index++){
+        
+        my $colName = $headers[$index]; 
+        
+        if ( ! exists ( $masterheaders{$colName} ) ) { $masterheaders{$colName} = 1; }
+        
+        #if ( grep( /$colName/, @masterheaders ) ){} else{ push @masterheaders, $colName;} 
 
-      print WRITEOUT "$file,$line";
+      }
+      
+      
+    }else{
     
+      @numbers = split /,/, $line; 
+      
+      my $index = 0;
+      
+      for( $index=0; $index<$#headers; $index++){
+        
+        my $header = $headers[$index];
+        my $value  = $numbers[$index];
+
+        $data{$file}{$header}[$RowNumber] = $value; 
+        
+        
+        #if ( $RowNumber == 0 ) {debug_out (" > header: >$header<\n");}
+      }
+      
+      $data{$file}{"aaa-filename"}[$RowNumber] = $file; 
+      
+
+      $RowNumber++;
+      
     }
-    
-    
-  
-    
-    $LineNum++;     
     
   }
   
-  $FirstFile = 0; 
+  $NumRows{$file}=$RowNumber; 
   
-  close (READIN); 
-  
+  close (TSRESULTS);
+
+  print " done. ($RowNumber rows)\n";
 }
 
+# Compute header roe 
+my $headerRow = ""; 
+my $noCol = 0; 
+my $headerDone = 0; 
 
 
-close (WRITEOUT); 
+foreach my $column ( sort keys %masterheaders ){
+
+  $headerRow = $headerRow.$column.","; 
+  
+  print " - $noCol, $column \n "; 
+  $noCol ++; 
+
+}
+$headerRow = "$headerRow\n";
+
+
+my $content = ""; 
+
+for $file (@ARGV ){ 
+
+  print "Writing results for $file \n";
+
+  for (my $row = 0; $row < $NumRows{$file}; $row++ ){
+  #for (my $row = 0; $row < 5 ; $row++ ){
+    
+    foreach my $column ( sort keys %masterheaders ){
+      #print " $row > $column \n";
+      
+      
+      if (exists( $data{$file}{$column}[$RowNumber] ) ) { 
+       $content .=  $data{$file}{$column}[$row] ;  
+      }else{
+        $content .="null";
+      } 
+      $content .= ","
+       
+    }
+    $content .= "\n"; 
+  }
+}
+
+print WRITEOUT $headerRow; 
+print WRITEOUT $content; 
+
+close WRITEOUT; 
+
+
+
+
+  
+  
+#  $LineNum = 1; 
+#  
+#  open ( READIN, $file ); 
+#  
+#  while ( my $line = <READIN> ){
+#  
+#    if ( $FirstFile == 1 && $LineNum == 1 ) {
+#
+#      print WRITEOUT "Filename,$line";
+#      
+#    }elsif( $LineNum > 1 ) {
+#
+#      print WRITEOUT "$file,$line";
+#    
+#    }
+#    
+#    
+#  
+#    
+#    $LineNum++;     
+#    
+#  }
+#  
+#  $FirstFile = 0; 
+#  
+#  close (READIN); 
+#  
+#}
+#
+#
+#
+#close (WRITEOUT); 
 
